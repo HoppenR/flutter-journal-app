5e48ff307c23e3db6ed0aefcab592201753f91d0
Christoffer Lundell <christofferlundell@protonmail.com>
2024-11-05 23:07:46 +0100

    Move adding tags to a separate window

---
+++ lib/add_tag_form.dart
@@ -0,0 +1,147 @@
+// Vim: set shiftwidth=2 :
+import 'package:flutter/material.dart';
+
+enum TagType { boolean, list }
+
+class FullScreenTagForm extends StatefulWidget {
+  const FullScreenTagForm({super.key, required this.tagNames});
+
+  final Map<String, List<String>> tagNames;
+
+  @override
+  FullScreenTagFormState createState() => FullScreenTagFormState();
+}
+
+class FullScreenTagFormState extends State<FullScreenTagForm> {
+  final TextEditingController tagController = TextEditingController();
+  final List<TextEditingController> optionControllers = <TextEditingController>[
+    TextEditingController(),
+  ];
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TagType? selectedType;
+
+  bool get _isSaveButtonEnabled {
+    _formKey.currentState?.validate();
+    if (tagController.text.isEmpty) {
+      return false;
+    }
+    if (selectedType == null) {
+      return false;
+    }
+    if (selectedType == TagType.list) {
+      return optionControllers.any(
+        (TextEditingController controller) => controller.text.isNotEmpty,
+      );
+    }
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Add Tag'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: _isSaveButtonEnabled
+              ? () {
+                if (selectedType == TagType.list) {
+                  widget.tagNames[tagController.text] = optionControllers
+                    .map((TextEditingController controller) => controller.text)
+                    .where((String text) => text.isNotEmpty)
+                    .toList();
+                } else if(selectedType == TagType.boolean) {
+                  // TODO(Hop): Think through this, rather have small text +
+                  //            strikethrough when checked?
+                  //            Need to rethink the entire boolean type
+                  widget.tagNames[tagController.text] = <String>['✅', '❎'];
+                }
+                Navigator.of(context).pop(true);
+              }
+              : null,
+            child: const Text('Save'),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          onChanged: () => setState(() {}),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                controller: tagController,
+                decoration: const InputDecoration(hintText: 'Enter a tag'),
+                validator: (String? value) => value == null || value.isEmpty
+                  ? 'Tag is required'
+                  : null,
+              ),
+              DropdownButtonFormField<TagType>(
+                value: selectedType,
+                hint: const Text('Select tag type'),
+                items: const <DropdownMenuItem<TagType>>[
+                  DropdownMenuItem<TagType>(
+                    value: TagType.boolean,
+                    child: Text('Checkmark'),
+                  ),
+                  DropdownMenuItem<TagType>(
+                    value: TagType.list,
+                    child: Text('Options'),
+                  ),
+                ],
+                onChanged: (TagType? value) => setState(() {
+                  selectedType = value;
+                }),
+                validator: (TagType? value) => value == null
+                  ? 'Tag type is required'
+                  : null,
+              ),
+              if (selectedType == TagType.list)
+                ..._buildOptionFields(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildOptionFields() {
+    return <Widget>[
+      const SizedBox(height: 16),
+      const Text('Options', style: TextStyle(fontWeight: FontWeight.bold)),
+      ...optionControllers.asMap().entries.map(
+        (MapEntry<int, TextEditingController> entry) => Row(
+          children: <Widget>[
+            Expanded(
+              child: TextFormField(
+                controller: entry.value,
+                decoration: const InputDecoration(
+                  hintText: 'Enter an option',
+                ),
+                validator: (String? value) => value == null || value.isEmpty ?
+                  'Option is required'
+                  : null,
+                onChanged: (String? value) => setState(() {}),
+              ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.remove_circle),
+              onPressed: () => setState(() {
+                optionControllers.removeAt(entry.key);
+              }),
+            ),
+          ],
+        ),
+      ),
+      IconButton(
+        icon: const Icon(Icons.add),
+        onPressed: () => setState(() {
+          optionControllers.add(TextEditingController());
+        }),
+      ),
+    ];
+  }
+}
