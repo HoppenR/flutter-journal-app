806292629dbccdfc60acbbcfcf600853912f68d5
Christoffer Lundell <christofferlundell@protonmail.com>
2025-03-18 15:36:50 +0100

    refactor: change how dates/weeks/pages are calculated for better consistency
    (still no good support for locales whose weeks start with sundays)

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,28 @@ import 'package:flutter/material.dart';
 import 'src/journal.dart';
 import 'src/utility.dart';
 
-// TODO(Christoffer): [etc.] Make all these TODOs into github issues
-//                           learn workflow of making/fixing github issues/PRs
-//                           Also add README.md/images?
-
-// TODO(Christoffer): [settings] Use monocolor emojis for "stack-icon" of book
-//                               and crescent moon. NotoEmoji maybe?
-//                               alternatively the delete button should be made
-//                               into purple/magenta.
-
-// TODO(Christoffer): [etc.] Implement routes for back-button popping states
 // TODO(Christoffer): [tag] Use tagDataID in AppliedTagData instead of storing
 //                    the entire tagData each time (see local storage / userprefs)
-
-// TODO(Christoffer): [tag] Use Set<int> for multiselection?
-
-// TODO(Christoffer): [graph] Display overviews of weeks/months
-
 // TODO(Christoffer): [add-tag-form] Ensure tag names are unique.
 //                    Later the name will tie appliedTagData to TagData instead
 //                    of copying the entire TagData each time
 //                    src/add_tag_form.dart:39
+// TODO(Christoffer): [tag] Use Set<int> for multiselection?
+
+// ---
+
+// TODO(Christoffer): [etc.] Make all these TODOs into github issues learn
+//                    workflow of making/fixing github issues/PRs
+//                    Also add README.md/images?
+
+// TODO(Christoffer): [settings] Use images/icons for "stack-icon" of book
+//                    and crescent moon. No need to bundle expensive truetype
+//                    fonts for 2 icons.
+
+// TODO(Christoffer): [etc.] Implement routes for back-button popping states
+
+// TODO(Christoffer): [graph] Display overviews of weeks/months
+
 // TODO(Christoffer): [add-tag-form] Add menstruation related icons
 // TODO(Christoffer): [add-tag-form] Add ImageIcons for emojis?
 
@@ -31,13 +32,12 @@ import 'src/utility.dart';
 //                    week (see twitch date picker for past broadcasts)
 
 // TODO(Christoffer): [tag-overview] use ChoiceChip over TextButton with colored
-//                                   styles
+//                    styles
 
 // ----- Check With Luunie First -----
 
 // TODO(Christoffer): [tag-overview] Highlight the relevant tag when opening the
-//                                   tag-overview, based on what square the user
-//                                   pressed
+//                    tag-overview, based on what square the user pressed
 
 void main() async {
   runApp(const InitializationWidget());
