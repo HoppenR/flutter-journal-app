7de68b7ceaeca90fcf2fae9745a1dfcb3de8c98f
Christoffer Lundell <christofferlundell@protonmail.com>
2026-01-17 14:21:27 +0100

    improve flake.nix

--- flake.nix
+++ flake.nix
@@ -23,13 +23,28 @@
               pkgs.git
               pkgs.unzip
               (pkgs.writeShellScriptBin "build-web" ''
-                exec flutter build web --base-href /journal/ --release "$@"
+                exec flutter build web --base-href /journal/ --wasm --release "$@"
               '')
 
               pkgs.jdk17 # For Android
             ];
 
             shellHook = ''
+              function cleanup() {
+                echo "󰃢 Cleaning up Flutter/Dart artifacts..."
+                set -x
+                if [[ -d "$HOME/.dart-tool" ]]; then
+                  rm --recursive --interactive=once "$HOME/.dart-tool"
+                fi
+                if [[ -d "$HOME/.pub-cache" ]]; then 
+                  rm --recursive --interactive=once "$HOME/.pub-cache"
+                fi
+                if [[ -f "$HOME/.flutter" ]]; then
+                  rm --interactive "$HOME/.flutter"
+                fi
+              }
+              trap cleanup EXIT
+
               echo "Using Flutter from: ${pkgs.flutter338}"
               flutter --version
             '';
